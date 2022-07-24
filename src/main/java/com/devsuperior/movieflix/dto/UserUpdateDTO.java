5abd7bb997b1c211fa9275a466d0dda7d3f82360
robertsouzza <rcsouzza@gmail.com>
2022-07-24 19:52:45 -0400

    insert e user DTO, validation

---
+++ src/main/java/com/devsuperior/movieflix/dto/UserUpdateDTO.java
@@ -0,0 +1,10 @@
+package com.devsuperior.movieflix.dto;
+
+import com.devsuperior.movieflix.services.validation.UserUpdateValid;
+
+@UserUpdateValid
+public class UserUpdateDTO extends UserDTO {
+    private static final long serialVersionUID = 1L;
+
+
+}
