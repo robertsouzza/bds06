ad478b373eda8c86246474fa76ca4d2c8676cd19
robertsouzza <rcsouzza@gmail.com>
2022-07-24 20:06:05 -0400

    Gen, Movie and Review repository

---
+++ src/main/java/com/devsuperior/movieflix/repositories/MovieRepository.java
@@ -0,0 +1,19 @@
+package com.devsuperior.movieflix.repositories;
+
+import com.devsuperior.movieflix.entities.Genre;
+import com.devsuperior.movieflix.entities.Movie;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+//import java.util.List;
+
+public interface MovieRepository extends JpaRepository<Movie, Long> {
+    @Query("SELECT obj FROM Movie obj WHERE:genre IS NULL OR :genre = obj.genre")
+    Page<Movie> find(Genre genre, Pageable pageable);
+
+    @Query("SELECT obj FROM Movie obj WHERE :genre IS NULL OR obj.genre = :genre ORDER BY obj.title ASC")
+    Page<Movie> searchAll(Genre genre, Pageable pageable);
+
+}
